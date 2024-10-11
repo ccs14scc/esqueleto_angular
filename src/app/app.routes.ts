2098264f54b57be88138b91143fb8447c2623b59
claudio castillo <claudio.castillo.schonffeldt@cftmail.cl>
2024-10-10 21:42:40 -0400

    primer uso de angular, y otros elementos adicionales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { RegistrateComponent } from './componentes/registrate/registrate.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "inicio",
+        component: InicioComponent
+    },
+    {
+        path: "contacto",
+        component: ContactoComponent
+    },
+    {
+        path: "registrate",
+        component: RegistrateComponent
+    },
+];
